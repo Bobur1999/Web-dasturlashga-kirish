ab188fad66f744b28b8f33524dfe1cd1fde0a280
Bobur <Boburbekrustamov1999@gmail.com>
2020-05-01 11:37:51 +0500

    yangiliklar chiqarish front qismi

--- resources/views/sport.blade.php
+++ resources/views/sport.blade.php
@@ -9,13 +9,140 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-              <h1>YANGILIKLAR</h1>
+              <h1>Sport yangiliklari</h1>
               <span class="subheading">eng so'nggilari bizda</span>
           </div>
         </div>
       </div>
     </div>
   </header>
-    sport yangiliklar!
+    
+  <div class="container">
+    <div class="row">
+        
+        <div class="col-lg-9">
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-lg-3" >
+            <div class="blog_right_sidebar">
+                <aside class="single_sidebar_widget popular_post_widget">
+                    <h3 class="widget_title">Ko'p o'qilganlar</h3>
+                    <div class="media post_item">
+                        <img width="80px" src="thumb" alt="post">
+                        <div class="media-body">
+                            <a href="">
+                            <h3>title</h3>
+                            </a>
+                            <p>
+                            <i class="fa fa-eye">12</i>
+                            ||
+                            <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="media post_item">
+                        <img width="80px" src="thumb" alt="post">
+                        <div class="media-body">
+                            <a href="">
+                            <h3>title</h3>
+                            </a>
+                            <p>
+                            <i class="fa fa-eye">12</i>
+                            ||
+                            <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="media post_item">
+                        <img width="80px" src="thumb" alt="post">
+                        <div class="media-body">
+                            <a href="">
+                            <h3>title</h3>
+                            </a>
+                            <p>
+                            <i class="fa fa-eye">12</i>
+                            ||
+                            <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="media post_item">
+                        <img width="80px" src="thumb" alt="post">
+                        <div class="media-body">
+                            <a href="">
+                            <h3>title</h3>
+                            </a>
+                            <p>
+                            <i class="fa fa-eye">12</i>
+                            ||
+                            <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+                            </p>
+                        </div>
+                    </div>                          
+                </aside>
+            </div>
+        </div>         
+
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <a href="#" class=""><h5 class="card-title">Special title treatment</h5></a>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <i class="fa fa-eye">12</i>
+                ||
+                <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <a href="#" class=""><h5 class="card-title">Special title treatment</h5></a>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <i class="fa fa-eye">12</i>
+                ||
+                <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <a href="#" class=""><h5 class="card-title">Special title treatment</h5></a>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <i class="fa fa-eye">12</i>
+                ||
+                <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <a href="#" class=""><h5 class="card-title">Special title treatment</h5></a>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <i class="fa fa-eye">12</i>
+                ||
+                <i class="fa fa-calendar">20:00 | 20.04.2020</i>
+            </div>
+            </div>
+        </div>
+
+    </div>
+  </div>
 
 @endsection
